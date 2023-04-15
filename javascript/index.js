ced0fbfd0fe9125da830b15620cedaa243d08098
Taiwo Adeniji Adebare <nijitaye@gmail.com>
2023-04-15 23:22:07 +0100

    The JavaScript
    This folder contains the JavaScript file for the Website.

---
+++ javascript/index.js
@@ -0,0 +1,272 @@
+const divContainer = document.querySelector('.barshowhide');
+const barimg2 = document.querySelector('.img1');
+const barimg = document.querySelector('.img2');
+let isClicked = true;
+
+let showOrHide = function(){
+    if(isClicked){
+        divContainer.style.display = 'block';
+        barimg.style.display = 'inline';
+        barimg2.style.display = 'none';
+        divContainerT.style.display = 'none'
+        barimg4.style.display = 'none';
+        barimg3.style.display = 'inline';
+        divContainerK.style.display = 'none';
+        barimg6.style.display = 'none';
+        barimg5.style.display = 'inline';
+        divContainerV.style.display = 'none';
+        barimg8.style.display = 'none';
+        barimg7.style.display = 'inline';
+        divContainerD.style.display = 'none';
+        barimg10.style.display = 'none';
+        barimg9.style.display = 'inline';
+        divContainerE.style.display = 'none';
+        barimg12.style.display = 'none';
+        barimg11.style.display = 'inline';
+        
+        isClicked = false
+    }else {
+        divContainer.style.display = 'none';
+        barimg.style.display = 'none';
+        barimg2.style.display = 'inline';
+        isClicked = true
+    }
+}
+
+
+
+const divContainerT = document.querySelector('.barshowhideT');
+const barimg3 = document.querySelector('.img3');
+const barimg4 = document.querySelector('.img4');
+let isClickedT = true;
+
+let showOrHideT = function(){
+    if(isClickedT){
+        divContainerT.style.display = 'block';
+        barimg4.style.display = 'inline';
+        barimg3.style.display = 'none';
+        divContainer.style.display = 'none';
+        barimg.style.display = 'none';
+        barimg2.style.display = 'inline';
+        divContainerK.style.display = 'none';
+        barimg6.style.display = 'none';
+        barimg5.style.display = 'inline';
+        divContainerV.style.display = 'none';
+        barimg8.style.display = 'none';
+        barimg7.style.display = 'inline';
+        divContainerD.style.display = 'none';
+        barimg10.style.display = 'none';
+        barimg9.style.display = 'inline';
+        divContainerE.style.display = 'none';
+        barimg12.style.display = 'none';
+        barimg11.style.display = 'inline';
+        
+        isClickedT = false
+    }else {
+        divContainerT.style.display = 'none';
+        barimg4.style.display = 'none';
+        barimg3.style.display = 'inline';
+        isClickedT = true
+
+    }
+}
+
+const divContainerK = document.querySelector('.barshowhideK');
+const barimg5 = document.querySelector('.img5');
+const barimg6 = document.querySelector('.img6');
+let isClickedK = true;
+
+let showOrHideK = function(){
+    if(isClickedK){
+        divContainerK.style.display = 'block';
+        barimg6.style.display = 'inline';
+        barimg5.style.display = 'none';
+        divContainerT.style.display = 'none';
+        barimg4.style.display = 'none';
+        barimg3.style.display = 'inline';
+        divContainer.style.display = 'none';
+        barimg.style.display = 'none';
+        barimg2.style.display = 'inline';
+        divContainerV.style.display = 'none';
+        barimg8.style.display = 'none';
+        barimg7.style.display = 'inline';
+        divContainerD.style.display = 'none';
+        barimg10.style.display = 'none';
+        barimg9.style.display = 'inline';
+        divContainerE.style.display = 'none';
+        barimg12.style.display = 'none';
+        barimg11.style.display = 'inline';
+
+        isClickedK = false
+    }else {
+        divContainerK.style.display = 'none';
+        barimg6.style.display = 'none';
+        barimg5.style.display = 'inline';
+        isClickedK = true
+
+    }
+}
+
+const divContainerV = document.querySelector('.barshowhideV');
+const barimg7 = document.querySelector('.img7');
+const barimg8 = document.querySelector('.img8');
+let isClickedV = true;
+
+let showOrHideV = function(){
+    if(isClickedV){
+        divContainerV.style.display = 'block';
+        barimg8.style.display = 'inline';
+        barimg7.style.display = 'none';
+        divContainerK.style.display = 'none';
+        barimg6.style.display = 'none';
+        barimg5.style.display = 'inline';
+        divContainerT.style.display = 'none';
+        barimg4.style.display = 'none';
+        barimg3.style.display = 'inline';
+        divContainer.style.display = 'none';
+        barimg.style.display = 'none';
+        barimg2.style.display = 'inline';
+        divContainerD.style.display = 'none';
+        barimg10.style.display = 'none';
+        barimg9.style.display = 'inline';
+        divContainerE.style.display = 'none';
+        barimg12.style.display = 'none';
+        barimg11.style.display = 'inline';
+        
+        isClickedV = false
+    }else {
+        divContainerV.style.display = 'none';
+        barimg8.style.display = 'none';
+        barimg7.style.display = 'inline';
+        isClickedV = true
+
+    }
+}
+
+const divContainerD = document.querySelector('.barshowhideD');
+const barimg9 = document.querySelector('.img9');
+const barimg10 = document.querySelector('.img10');
+let isClickedD = true;
+
+let showOrHideD = function(){
+    if(isClickedD){
+        divContainerD.style.display = 'block';
+        barimg10.style.display = 'inline';
+        barimg9.style.display = 'none';
+        divContainerV.style.display = 'none';
+        barimg8.style.display = 'none';
+        barimg7.style.display = 'inline';
+        divContainerK.style.display = 'none';
+        barimg6.style.display = 'none';
+        barimg5.style.display = 'inline';
+        divContainerT.style.display = 'none';
+        barimg4.style.display = 'none';
+        barimg3.style.display = 'inline';
+        divContainer.style.display = 'none';
+        barimg.style.display = 'none';
+        barimg2.style.display = 'inline';
+        divContainerE.style.display = 'none';
+        barimg12.style.display = 'none';
+        barimg11.style.display = 'inline';
+
+        isClickedD = false
+    }else {
+        divContainerD.style.display = 'none';
+        barimg10.style.display = 'none';
+        barimg9.style.display = 'inline';
+        isClickedD = true
+
+    }
+}
+
+const divContainerE = document.querySelector('.barshowhideE');
+const barimg11 = document.querySelector('.img11');
+const barimg12 = document.querySelector('.img12');
+let isClickedE = true;
+
+let showOrHideE = function(){
+    if(isClickedE){
+        divContainerE.style.display = 'block';
+        barimg12.style.display = 'inline';
+        barimg11.style.display = 'none';
+        divContainerD.style.display = 'none';
+        barimg10.style.display = 'none';
+        barimg9.style.display = 'inline';
+        divContainerV.style.display = 'none';
+        barimg8.style.display = 'none';
+        barimg7.style.display = 'inline';
+        divContainerK.style.display = 'none';
+        barimg6.style.display = 'none';
+        barimg5.style.display = 'inline';
+        divContainerT.style.display = 'none';
+        barimg4.style.display = 'none';
+        barimg3.style.display = 'inline';
+        divContainer.style.display = 'none';
+        barimg.style.display = 'none';
+        barimg2.style.display = 'inline';
+
+
+        isClickedE = false
+    }else {
+        divContainerE.style.display = 'none';
+        barimg12.style.display = 'none';
+        barimg11.style.display = 'inline';
+        isClickedE = true
+
+    }
+}
+
+const divContainertxt = document.querySelector('.input1onclik');
+let isClickedTxt = true;
+
+let showOrHideTxt = function(){
+    if(isClickedTxt){
+        divContainertxt.style.display = 'block';
+        isClickedTxt = false;
+    }
+}
+
+const divContainertxt2 = document.querySelector('.input2onclik');
+let isClickedTxt2 = true;
+
+let showOrHideTxt2 = function(){
+    if(isClickedTxt2){
+        divContainertxt2.style.display = 'block';
+        spanTogg.style.display = 'block';
+        isClickedTxt2 = false;
+    }
+}
+
+const spanTogg = document.querySelector('.span1');
+const spanTogg2 = document.querySelector('.span2');
+var state= false;
+function toggle(){
+    if(state){
+        document.getElementById("password").
+        setAttribute("type", "password");
+        spanTogg.style.display = 'block';
+        state = false;
+
+    } else{
+        document.getElementById("password").
+        setAttribute("type", "text");
+        spanTogg.style.display = 'none';
+        spanTogg2.style.display = 'block';
+        state = true;
+    }
+}
+var state2 = false; {
+    function toghide() {
+        if(state){
+            document.getElementById("password").
+            setAttribute("type", "password");
+            spanTogg.style.display = 'block';
+            spanTogg2.style.display = 'none';
+            state = false;
+        }else{
+            spanTogg.style.display = 'block';
+            spanTogg2.style.display = 'none';
+
+        }
+    }
+}
